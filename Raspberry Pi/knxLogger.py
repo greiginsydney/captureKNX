171564200cbf914fb09fedddbf54616123326460
Greig Sheridan <greiginsydney@users.noreply.github.com>
2024-08-27 13:02:36 +1000

    Added GNU licence header & info

--- Raspberry Pi/knxLogger.py
+++ Raspberry Pi/knxLogger.py
@@ -1,3 +1,17 @@
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# This script is part of the knxLogger project, which logs all KNX bus traffic to an InfluxDb for reporting via Grafana
+# https://github.com/greiginsydney/knxLogger
+# https://greiginsydney.com/knx-logger
+
+
 import asyncio
 import json                         # For sending to telegraf
 import knxdclient
