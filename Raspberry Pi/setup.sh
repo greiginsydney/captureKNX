1e33491cac16d41673ffbe591e12ade6c4a228ea
Greig Sheridan <greiginsydney@users.noreply.github.com>
2025-01-06 12:52:38 +1100

    Reinstate apt-get update before installing telegraf

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -351,7 +351,7 @@ setup1()
 		curl -s https://repos.influxdata.com/influxdata-archive.key > influxdata-archive.key
 		echo '943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515 influxdata-archive.key' | sha256sum -c && cat influxdata-archive.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
 		echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
-		sudo apt-get install telegraf -y
+		apt-get update && sudo apt-get install telegraf -y
 	fi
 
 	# Suppress noisy error message in OOB telegraf.service (n/a on Debian/Raspbian):
