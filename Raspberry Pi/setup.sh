1c125313c69b790e6b7f81db6bd5a0219914b009
Greig Sheridan <greiginsydney@users.noreply.github.com>
2025-01-06 13:04:18 +1100

    Reinstated apt-get update when installing grafana

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -407,7 +407,7 @@ setup1()
 		mkdir -p /etc/apt/keyrings/
 		wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 		echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-		apt-get install grafana-enterprise -y
+		apt-get update && apt-get install grafana-enterprise -y
 	fi
 
 	# grafana-source.yaml:
