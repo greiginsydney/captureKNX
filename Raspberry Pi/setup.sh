0b55692020f456e1ef30947ca0e692c1ce1d4543
Greig Sheridan <greiginsydney@users.noreply.github.com>
2026-02-01 17:45:04 +1100

    Updated telegraf install key & steps

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -358,11 +358,16 @@ setup1()
 		fi
 	else
 		echo -e "\n"$GREEN"Installing telegraf"$RESET""
+		# Install steps from here: https://www.influxdata.com/downloads/
 		rm -rf /home/$SUDO_USER/staging/telegraf
 		mkdir -pv /home/$SUDO_USER/staging/telegraf
 		cd /home/${SUDO_USER}/staging/telegraf/
-		curl -s https://repos.influxdata.com/influxdata-archive.key > influxdata-archive.key
-		echo '943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515 influxdata-archive.key' | sha256sum -c && cat influxdata-archive.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
+		
+		# influxdata-archive.key GPG fingerprint:
+		#   Primary key fingerprint: 24C9 75CB A61A 024E E1B6  3178 7C3D 5715 9FC2 F927
+		#   Subkey fingerprint:      9D53 9D90 D332 8DC7 D6C8  D3B9 D8FF 8E1F 7DF8 B07E
+		wget -q https://repos.influxdata.com/influxdata-archive.key
+		gpg --show-keys --with-fingerprint --with-colons ./influxdata-archive.key 2>&1 | grep -q '^fpr:\+24C975CBA61A024EE1B631787C3D57159FC2F927:$' && cat influxdata-archive.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
 		echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 		apt-get update && apt-get install telegraf -y
 	fi
